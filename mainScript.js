fba2cc5fc1ac223e3bce62b093c2e982297d2e52
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-12-14 08:10:59 -0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -2,6 +2,7 @@ var body = document.querySelector('body');
 
 var petImg = document.createElement("img");
 petImg.src="https://static.vecteezy.com/system/resources/previews/010/265/386/original/cute-3d-robot-smart-png.png";
+petImg.style.width="200px";
 petImg.style.zIndex="99";
 body.appendChild(petImg);
 
