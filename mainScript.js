6733d7113471e2d082bb0110347f913e0ec8fa35
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-12-13 19:09:30 -0800

    Create mainScript.js

---
+++ mainScript.js
@@ -0,0 +1,8 @@
+var body = document.querySelector('body');
+
+var petImg = document.createElement("img");
+petImg.src="https://orangemushroom.files.wordpress.com/2022/01/stimulate-effect.gif";
+petImg.style.zIndex="99";
+body.appendChild(petImg);
+
+
