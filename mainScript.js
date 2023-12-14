08e11ed2772875d01a71601d9586497840a9934f
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-12-14 08:34:42 -0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -2,7 +2,7 @@ var body = document.querySelector('body');
 
 var pet = document.createElement("div");
 pet.innerHTML='<div class="body"><div class="head"></div><div class="stomach"></div><div class="hands"><div class="l-hand"></div><div class="r-hand"></div></div><div class="legs"><div class="l-leg"></div><div class="r-leg"></div></div></div>'
-
+alert("hi")
 body.appendChild(pet);
 
 
